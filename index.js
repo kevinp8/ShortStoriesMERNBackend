54737706dd3bc4eec09eaa8e59b621ddb9f4a165
Kevin <kevdev.yr6re@slmail.me>
2022-12-09 01:14:32 -0800

    now api runs then connects to the database

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ import storiesRoutes from "./routes/stories.js";
 
 dotenv.config({ path: "./config/config.env" });
 
-connectDB();
 const app = express();
-
-app.use(cors());
-app.use(express.json());
 
 app.listen(process.env.PORT, () => {
   console.log("server running");
 });
+connectDB();
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/stories", storiesRoutes);
